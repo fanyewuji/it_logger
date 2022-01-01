60fc2e12f7cb1b1278bc0d50087665c596234f43
fanyewuji <fanyewuji@163.com>
2022-01-01 10:25:22 -0500

    create searchBar and Logs

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js'
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // initialize materialize js
+    M.AutoInit();
+  })
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  );
+}
+
+export default App;
