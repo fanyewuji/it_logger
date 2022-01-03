8605e1ef6b80ec042d352c306fc37e8624c99c9a
fanyewuji <fanyewuji@163.com>
2022-01-03 08:18:55 -0500

    AddBtn component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
@@ -14,6 +15,7 @@ const App = () => {
     <>
       <SearchBar />
       <div className="container">
+        <AddBtn />
         <Logs />
       </div>
     </>
